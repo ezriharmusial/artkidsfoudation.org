a39c9e9983d7bf00ee39cde7d53beee689720125
ezriharmusial <ezriharmusial@gmail.com>
2022-08-11 22:41:06 +0200

    Responsive complete

--- src/layouts/default.svelte
+++ src/layouts/default.svelte
@@ -9,22 +9,21 @@
 </svelte:head>
 
 	<header class="hero is-fullheight is-warning is-bold">
-		<div class="hero-body">
-				{#if thumbnail}
+	<div class="hero-body">
+
+		{#if thumbnail}
 				<figure class="image">
 					<img src="{thumbnail}" alt="{title}">
 				</figure>
 				{/if}
-		</div>
+	</div>
 		<div class="hero-foot">
 		<div class="container">
-			<div class="section">
 			<div class="notification is-dark">
 				<p class="title is-size-4-mobile has-text-white">{title}</p>
 				{#if subtitle}
 				<p class="subtitle is-size-5-mobile has-text-white">{subtitle}</p>
 				{/if}
-			</div>
 			</div>
 		</div>
 		</div>
@@ -40,6 +39,7 @@
 <style global lang="scss">
 	.hero.is-fullheight {
 		min-height: 75vh !important;
+		max-height: 75vh !important;
 	}
 
 	.image {
@@ -56,6 +56,22 @@
 	@media screen and (max-width:1024px) {
 		.hero.is-fullheight {
 			min-height: 50vh !important;
+			max-height: 50vh !important;
+
+			.image {
+				max-height: 50vh !important;
+			}
+
+			.hero-body {
+				padding-top: 0;
+			}
 		}
+	}
+
+	.notification {
+		display:inline-block;
+		position: absolute;
+		bottom: 0;
+		z-index: 15;
 	}
 </style>
