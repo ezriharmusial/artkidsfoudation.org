90751e62b189d921ad98a44e5dc3e53732cfaf4c
ezriharmusial <ezriharmusial@gmail.com>
2022-08-11 18:48:45 +0200

    Default template styles

--- src/layouts/default.svelte
+++ src/layouts/default.svelte
@@ -9,12 +9,12 @@
 	<title>{title}</title>
 </svelte:head>
 
-	<header class="hero is-medium is-warning is-bold">
-		<div class="hero-body py-0">
+	<header class="hero is-fullheight is-warning is-bold">
+		<div class="hero-body">
 			{#if thumbnail}
 			<figure class="image">
 				<img src="{thumbnail}" alt="{title}">
-				<figcaption class="notification is-dark" style="position:absolute; top: 7rem;">
+				<figcaption class="notification is-dark">
 					<p class="title is-size-4-mobile has-text-white">{title}</p>
 					{#if subtitle}
 					<p class="subtitle is-size-5-mobile has-text-white">{subtitle}</p>
@@ -34,26 +34,25 @@
 
 	<main class="container">
 		<div class="box content">
-			<p class="title is-size-3-mobile">{title}</p>
-			{#if subtitle}
-			<p class="subtitle is-size-4-mobile">{subtitle}</p>
-			{/if}
 			<slot />
 		</div>
 	</main>
 
 
 <style global lang="scss">
-	.image {
-		position: relative;
-		max-height: 60vh;
-		overflow: hidden;
-	}
+	.is-fullheight {
+		margin-bottom: -15vh;
+		margin-top: calc(-15vh + 52px );
 
-	figcaption {
-		position: absolute;
-		left: 0;
-		top: 40vh;
-		padding: 0.5rem;
+		.image {
+			flex-grow: 1;
+			max-height: 60vh;
+			overflow: hidden;
+
+			figcaption {
+				position: absolute;
+				bottom: 0;
+			}
+		}
 	}
 </style>
